ef734ad2fe9eb29878f77314df106cab47272c28
cemcerok <vladmafia666@gmail.com>
2026-09-20 23:01:12 +0300

    Add task schemas and create endpoint

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException, status
+from app.schemas.task import TaskResponse, TaskCreate
 
 router = APIRouter()
 
@@ -16,11 +17,24 @@ tasks = [
     }
 ]
 
-@router.get("/tasks")
+@router.get("/tasks", response_model=list[TaskResponse])
 def get_tasks():
     return tasks
 
-@router.get("/tasks/{task_id}")
+@router.post("/tasks", status_code=status.HTTP_201_CREATED, response_model=TaskResponse)
+def create_task(task: TaskCreate):
+    id_list = list(i["id"] for i in tasks)
+    if len(id_list):
+        max_id = max(id_list)
+    else:
+        max_id = 0
+
+    task_dict = {"id": max_id + 1, **task.model_dump()}
+    tasks.append(task_dict)
+
+    return task_dict
+
+@router.get("/tasks/{task_id}", response_model=TaskResponse)
 def get_task_by_id(task_id: int):
     for task in tasks:
         if task["id"] == task_id:
